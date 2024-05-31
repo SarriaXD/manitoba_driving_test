34c65bb56d6bc371c99349fced52a2231f07bf3b
SarriaXD <sarria.qi.wang@gmail.com>
2024-05-31 11:38:28 -0500

    rewrite question_history_screen.dart using hooks.

--- lib/features/quizzes_history/screens/quizzes_history_screen.dart
+++ lib/features/quizzes_history/screens/quizzes_history_screen.dart
@@ -1,63 +1,62 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:manitoba_driving_test/features/quizzes_history/viewmodel/quiz_history_viewmodel.dart';
 
 import '../widgets/quiz_history/quiz_histories_animated_list.dart';
 
-class QuizzesHistoryScreen extends ConsumerStatefulWidget {
+class QuizzesHistoryScreen extends HookConsumerWidget {
   const QuizzesHistoryScreen({super.key, required this.scrollController});
   final ScrollController scrollController;
 
-  @override
-  ConsumerState<QuizzesHistoryScreen> createState() =>
-      _QuizzesHistoryScreenState();
-}
-
-class _QuizzesHistoryScreenState extends ConsumerState<QuizzesHistoryScreen>
-    with AutomaticKeepAliveClientMixin {
-  var canTriggerLoadMore = true;
-
-  @override
-  void initState() {
-    super.initState();
-    widget.scrollController.addListener(() async {
-      if (widget.scrollController.position.pixels >
-              widget.scrollController.position.maxScrollExtent + 50 &&
-          canTriggerLoadMore) {
-        canTriggerLoadMore = false;
-        await ref
-            .read(quizHistoryViewModelProvider(
-                    FirebaseAuth.instance.currentUser?.uid)
-                .notifier)
-            .loadMore();
-        // Delay 3 second to prevent multiple trigger
-        Future.delayed(const Duration(seconds: 2), () {
-          canTriggerLoadMore = true;
-        });
+  void userScrollListener(
+    WidgetRef ref,
+    ObjectRef<bool> canTriggerLoadMore,
+  ) {
+    useEffect(() {
+      void listener() async {
+        if (scrollController.position.pixels >
+                scrollController.position.maxScrollExtent + 50 &&
+            canTriggerLoadMore.value) {
+          canTriggerLoadMore.value = false;
+          await ref
+              .read(quizHistoryViewModelProvider(
+                      FirebaseAuth.instance.currentUser?.uid)
+                  .notifier)
+              .loadMore();
+          // Delay 3 second to prevent multiple trigger
+          Future.delayed(const Duration(seconds: 2), () {
+            canTriggerLoadMore.value = true;
+          });
+        }
       }
-    });
+
+      scrollController.addListener(listener);
+      return () => scrollController.removeListener(listener);
+    }, [scrollController]);
   }
 
   @override
-  Widget build(BuildContext context) {
-    super.build(context);
+  Widget build(BuildContext context, WidgetRef ref) {
+    useAutomaticKeepAlive(wantKeepAlive: true);
+    final canTriggerLoadMore = useRef(true);
+    userScrollListener(ref, canTriggerLoadMore);
     final state = ref.watch(
         quizHistoryViewModelProvider(FirebaseAuth.instance.currentUser?.uid));
     if (state.isFirstLoading) {
       return const Center(child: CircularProgressIndicator());
     }
     return QuizHistoriesAnimatedList(
-      scrollController: widget.scrollController,
-      unfinishedHistories: state.unfinishedHistories,
-      finishedHistoriesOrDate: state.finishedHistoriesOrDate,
-      isLoadingMore: state.isLoadingMore,
-      onReloadHistories: _onReloadHistories,
-      onHistoryRemoved: _onHistoryRemoved,
-    );
+        scrollController: scrollController,
+        unfinishedHistories: state.unfinishedHistories,
+        finishedHistoriesOrDate: state.finishedHistoriesOrDate,
+        isLoadingMore: state.isLoadingMore,
+        onReloadHistories: _onReloadHistories,
+        onHistoryRemoved: (historyId) => _onHistoryRemoved(ref, historyId));
   }
 
-  void _onReloadHistories() {
+  void _onReloadHistories(WidgetRef ref) {
     ref
         .read(
             quizHistoryViewModelProvider(FirebaseAuth.instance.currentUser?.uid)
@@ -65,14 +64,11 @@ class _QuizzesHistoryScreenState extends ConsumerState<QuizzesHistoryScreen>
         .reloadHistories();
   }
 
-  void _onHistoryRemoved(int historyId) {
+  void _onHistoryRemoved(WidgetRef ref, int historyId) {
     ref
         .read(
             quizHistoryViewModelProvider(FirebaseAuth.instance.currentUser?.uid)
                 .notifier)
         .removeHistory(historyId);
   }
-
-  @override
-  bool get wantKeepAlive => true;
 }
