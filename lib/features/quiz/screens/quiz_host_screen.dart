653bd81979d8c6626ae93569a4bea2cee113b3e1
SarriaXD <sarria.qi.wang@gmail.com>
2024-06-01 03:55:54 -0500

    rewrite quiz_host_screen.dart using hooks.

--- lib/features/quiz/screens/quiz_host_screen.dart
+++ lib/features/quiz/screens/quiz_host_screen.dart
@@ -1,71 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:manitoba_driving_test/features/quiz/screens/quiz_question_screen.dart';
 import 'package:manitoba_driving_test/features/quiz/screens/quiz_result_screen.dart';
 import 'package:manitoba_driving_test/features/quizzes_history/screens/quiz_history_detail_screen.dart';
 
 import 'quiz_question_quantity_screen.dart';
 
-class QuizHostScreen extends StatefulWidget {
+class QuizHostScreen extends HookWidget {
   const QuizHostScreen({super.key, this.historyId});
   final int? historyId;
 
-  @override
-  State<QuizHostScreen> createState() => _QuizHostScreenState();
-}
-
-class _QuizHostScreenState extends State<QuizHostScreen> {
-  late Widget currentScreen;
-  @override
-  void initState() {
-    if (widget.historyId == null) {
-      currentScreen = generateQuizQuestionQuantityScreen();
-    } else {
-      currentScreen = generateQuizQuestionScreen(historyId: widget.historyId!);
-    }
-    super.initState();
-  }
-
-  Widget generateQuizQuestionQuantityScreen() {
+  Widget generateQuizQuestionQuantityScreen(
+      ValueNotifier<Widget> currentScreen) {
     return QuizQuestionQuantityScreen(
       onNext: () {
-        setState(() {
-          currentScreen = generateQuizQuestionScreen(historyId: null);
-        });
+        currentScreen.value = generateQuizQuestionScreen(
+          historyId: null,
+          currentScreen: currentScreen,
+        );
       },
     );
   }
 
-  Widget generateQuizQuestionScreen({required int? historyId}) {
+  Widget generateQuizQuestionScreen(
+      {required int? historyId, required ValueNotifier<Widget> currentScreen}) {
     return QuizQuestionScreen(
       historyId: historyId,
       onNavigateToResultScreen: (newHistoryId) {
-        setState(
-          () {
-            currentScreen = generateQuizResultScreen(newHistoryId);
-          },
+        currentScreen.value = generateQuizResultScreen(
+          historyId: newHistoryId,
+          currentScreen: currentScreen,
         );
       },
     );
   }
 
-  Widget generateQuizResultScreen(int historyId) {
+  Widget generateQuizResultScreen(
+      {required int historyId, required ValueNotifier<Widget> currentScreen}) {
     return QuizResultScreen(
       historyId: historyId,
       onReviewQuiz: () {
-        setState(
-          () {
-            currentScreen = generateQuizHistoryDetailScreen(
-              historyId: historyId,
-              needReloadHistories: true,
-            );
-          },
+        currentScreen.value = generateQuizHistoryDetailScreen(
+          historyId: historyId,
+          needReloadHistories: true,
         );
       },
       onRetryQuiz: () {
-        setState(
-          () {
-            currentScreen = generateQuizQuestionScreen(historyId: null);
-          },
+        currentScreen.value = generateQuizQuestionScreen(
+          historyId: null,
+          currentScreen: currentScreen,
         );
       },
     );
@@ -83,9 +66,19 @@ class _QuizHostScreenState extends State<QuizHostScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final currentScreen = useState<Widget>(const SizedBox());
+    useEffect(() {
+      if (historyId == null) {
+        currentScreen.value = generateQuizQuestionQuantityScreen(currentScreen);
+      } else {
+        currentScreen.value = generateQuizQuestionScreen(
+            historyId: historyId, currentScreen: currentScreen);
+      }
+      return null;
+    }, []);
     return Scaffold(
       body: AnimatedSwitcher(
-        key: ValueKey(widget.historyId),
+        key: ValueKey(historyId),
         duration: Durations.extralong4,
         switchInCurve: Curves.fastLinearToSlowEaseIn,
         switchOutCurve: Curves.fastOutSlowIn,
@@ -102,7 +95,7 @@ class _QuizHostScreenState extends State<QuizHostScreen> {
             ),
           );
         },
-        child: currentScreen,
+        child: currentScreen.value,
       ),
     );
   }
